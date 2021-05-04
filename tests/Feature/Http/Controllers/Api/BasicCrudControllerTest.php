38bf103fc2408bc611eff13887f79317cede733d
Pedr Rabelo <pnarabelo@gmail.com>
2021-05-04 00:20:50 -0300

    Criação dos stubs

---
+++ tests/Feature/Http/Controllers/Api/BasicCrudControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*Comando para testar: ./vendor/bin/phpunit tests */
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Tests\Stubs\Controllers\CategoryControllerStub;
+use Tests\Stubs\Models\CategoryStub;
+use Tests\TestCase;
+
+class BasicCrudControllerTest extends TestCase
+{
+    private $controller;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        CategoryStub::dropTable();
+        CategoryStub::createTable();
+        $this->controller = new CategoryControllerStub();
+    }
+
+    protected function tearDown(): void
+    {
+        CategoryStub::dropTable();
+        parent::tearDown();
+    }
+
+    public function testIndex()
+    {
+        $category = CategoryStub::create(['name' => 'test_name', 'description' => 'test_description']);
+        $controller = new CategoryControllerStub();
+        $result = $this->controller->index()->toArray();
+        $this->assertEquals([$category->toArray()], $result);
+    }
+
+
+    public function testInvalidationDataInStore()
+    {
+        $this->expectException(ValidationException::class);
+
+        $request = \Mockery::mock(Request::class);
+        $request
+            ->shouldReceive('all')
+            ->once()
+            ->andReturn(['name' => '']);
+        $this->controller->store($request);
+    }
+}
