b07f9c2c3ac6bee6ab9af61de8cf7dc9e08bdb8a
Pedr Rabelo <pnarabelo@gmail.com>
2021-05-05 00:09:46 -0300

    Testes e CRUD de vídeo e castMember

--- app/Http/Controllers/Api/BasicCrudController.php
+++ app/Http/Controllers/Api/BasicCrudController.php
@@ -9,7 +9,10 @@ use Illuminate\Http\Request;
 abstract class BasicCrudController extends Controller
 {
     protected abstract function model();
+
     protected abstract function rulesStore();
+
+    protected abstract function rulesUpdate();
 
     public function index()
     {
@@ -18,6 +21,37 @@ abstract class BasicCrudController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request, $this->rulesStore());
+        $validatedData = $this->validate($request, $this->rulesStore());
+        $obj = $this->model()::create($validatedData);
+        $obj->refresh();
+        return $obj;
+    }
+
+    protected function findOrFail($id)
+    {
+        $model = $this->model();
+        $keyName = (new $model)->getRouteKeyName();
+        return $this->model()::where($keyName, $id)->firstOrFail();
+    }
+
+    public function show($id)
+    {
+        $obj = $this->findOrFail($id);
+        return $obj;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $obj = $this->findOrFail($id);
+        $validatedData = $this->validate($request, $this->rulesUpdate());
+        $obj->update($validatedData);
+        return $obj;
+    }
+
+    public function destroy($id)
+    {
+        $obj = $this->findOrFail($id);
+        $obj->delete();
+        return response()->noContent();
     }
 }
--- tests/Stubs/Controllers/CategoryControllerStub.php
+++ tests/Stubs/Controllers/CategoryControllerStub.php
@@ -3,9 +3,6 @@
 namespace Tests\Stubs\Controllers;
 
 use App\Http\Controllers\Api\BasicCrudController;
-use App\Http\Controllers\Controller;
-use App\Models\Category;
-use Illuminate\Http\Request;
 use Tests\Stubs\Models\CategoryStub;
 
 class CategoryControllerStub extends BasicCrudController
@@ -19,6 +16,15 @@ class CategoryControllerStub extends BasicCrudController
     {
         return [
             'name' => 'required|max:255',
+            'description' => 'nullable'
+        ];
+    }
+
+    protected function rulesUpdate()
+    {
+        return [
+            'name' => 'required|max:255',
+            'description' => 'nullable'
         ];
     }
 }
