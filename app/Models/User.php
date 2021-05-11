fbf2d08eecddc5440ddfcc01d6832ad953b78a6c
Pedr Rabelo <pnarabelo@gmail.com>
2021-05-10 23:10:03 -0300

    Finalizado auth com keycloak

---
+++ app/Models/User.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+
+class User implements Authenticatable
+{
+    protected $id;
+
+    protected $name;
+
+    protected $email;
+
+    protected $token;
+
+    public function __construct(string $id, string $name, string $email, string $token)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->token = $token;
+    }
+
+
+    public function getAuthIdentifierName()
+    {
+        return $this->email;
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->id;
+    }
+
+    public function getAuthPassword()
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function getRememberToken()
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function setRememberToken($value)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function getRememberTokenName()
+    {
+        throw new \Exception('Not implemented');
+    }
+}
