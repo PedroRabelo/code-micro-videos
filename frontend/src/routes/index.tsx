6881d16ad2598d36166b19d3202d045a43404844
Pedr Rabelo <pnarabelo@gmail.com>
2021-05-21 18:46:08 -0300

    Listagem de Categorias

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,34 @@
+import {RouteProps} from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import CategoryList from '../pages/category/PageList';
+
+export interface MyRouteProps extends RouteProps {
+    name: string;
+    label: string;
+}
+
+const routes : MyRouteProps[] = [
+    {
+        name: 'dashboard',
+        label: 'Dashboard',
+        path: '/',
+        component: Dashboard,
+        exact: true
+    },
+    {
+        name: 'categories.list',
+        label: 'Listar categorias',
+        path: '/categories',
+        component: CategoryList,
+        exact: true
+    },
+    {
+        name: 'categories.create',
+        label: 'Criar categorias',
+        path: '/categories/create',
+        component: CategoryList,
+        exact: true
+    },
+];
+
+export default routes;
